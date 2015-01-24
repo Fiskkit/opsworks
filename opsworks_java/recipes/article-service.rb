76d361e63281e7dede77bc92e54dd041198e3bc5
Ayinde Williams <ayinde@fiskkit.com>
2015-01-24 14:06:45 -0800

    deployment fix

--- opsworks_java/recipes/article-service.rb
+++ opsworks_java/recipes/article-service.rb
@@ -2,6 +2,11 @@ service 'article_server' do
   service_name node['opsworks_java']['service_name']
 
   case node[:platform_family]
+  when "ubuntu"
+    if node["platform_version"].to_f >= 9.10
+      provider Chef::Provider::Service::Upstart
+      supports :status => true, :restart => true, :reload => true
+    end
   when 'debian'
     supports :restart => true, :reload => false, :status => true
   when 'rhel'
